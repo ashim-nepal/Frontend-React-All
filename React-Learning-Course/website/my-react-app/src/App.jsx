f52fd1e6f49ab7b1dd290351e00b1ca2d26d432a
ashim-nepal <npashim015@gmail.com>
2024-08-14 18:35:26 +0545

    props basics in react library

--- React-Learning-Course/website/my-react-app/src/App.jsx
+++ React-Learning-Course/website/my-react-app/src/App.jsx
@@ -2,12 +2,16 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Card from "./Card";
 import Button from "./Button/Button"; // Styling with css, inline, external and modules
+import Student from "./Student";
 
 function App() {
   return(
     <> {/* Fragments tags required*/}
       <Header/>
       <Card/><br/>
+      <Student name="Patrik" age={21} isStudent={true}/><br/>
+      <Student name="Peter" age={57} isStudent={false}/>
+      <Student/>
       <Button/>
       <br/><br/><hr/>
       <Footer/>
