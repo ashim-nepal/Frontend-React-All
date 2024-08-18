81c5757b4db14d7cec15cdfd1314395d78e0a3dd
ashim-nepal <npashim015@gmail.com>
2024-08-18 21:25:39 +0545

    todo list app using react

--- React-Learning-Course/website/my-app-3/src/App.jsx
+++ React-Learning-Course/website/my-app-3/src/App.jsx
@@ -2,11 +2,14 @@ import ArrayObjectStateUpd from "./ArrayObjectStateUpd"
 import ArrayStateUpd from "./ArrayStateUpd"
 import ColorPicker from "./ColorPicker"
 import MyComponent from "./MyComponent"
+import ToDoList from "./ToDoList"
 
 function App() {
 
   return (
     <>
+    <div><ToDoList/></div>
+    <br/><br/><br/>
     <ArrayObjectStateUpd/><br/><br/>
     <ArrayStateUpd/><br/><br/>
     <MyComponent/>
