516d4ac2ffbc6babad06704451c38ec6698af1ba
ashim-nepal <npashim015@gmail.com>
2024-08-21 21:29:28 +0545

    stopwatch app in react

--- React-Learning-Course/website/my-app-4/src/App.jsx
+++ React-Learning-Course/website/my-app-4/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MyComponent from './MyComponent';
+import Stopwatch from './Stopwatch';
 
 function App() {
 
   return (
     <>
-    <MyComponent/>
+    <Stopwatch/>
+
+
+    {/* <MyComponent/> */}
     
     </>
   );
---
+++ React-Learning-Course/website/my-app-4/src/Stopwatch.jsx
@@ -0,0 +1,74 @@
+import React, {useState, useEffect, useRef} from "react";
+
+function Stopwatch(){
+
+    const [isRunning, setIsRunning]= useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+
+    useEffect(()=>{
+        if(isRunning){
+            
+                intervalIdRef.current = setInterval(()=>{
+                    setElapsedTime(Date.now() - startTimeRef.current)
+            }, 10);
+        }
+
+        return()=>{
+            clearInterval(intervalIdRef.current);
+        }
+
+    }, [isRunning]);
+
+    function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+
+    }
+
+    function stop(){
+        setIsRunning(false);
+
+    }
+
+    function reset(){
+        setElapsedTime(0);
+        setIsRunning(false);
+
+    }
+
+    function formatTime(){
+
+        let hours = Math.floor(elapsedTime / (1000 * 60 *60));
+        let minute = Math.floor(elapsedTime / (1000 *60) % 60)
+        let seconds = Math.floor(elapsedTime / (1000) % 60)
+        let milisecs = Math.floor(elapsedTime % 1000 / 10)
+
+        hours = String(hours).padStart(2, "0");
+        minute = String(minute).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milisecs = String(milisecs).padStart(2, "0");
+
+        return `${minute}:${seconds}:${milisecs}`;
+
+    }
+
+    return(
+        <div className="stopwatch">
+            <div className="display">
+                {formatTime()}
+            </div>
+
+            <div className="controls">
+                <button onClick={start} className="start-btn">Start</button>
+                <button onClick={stop} className="stop-btn">Stop</button>
+                <button onClick={reset} className="reset-btn">Reset</button>
+            </div>
+
+        </div>
+    );
+}
+
+export default Stopwatch;
